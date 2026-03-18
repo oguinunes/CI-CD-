21b0a44f7f9972d67493735b3b3ce01ac912f6d6
Guilherme Nunes <guilherme.ibnunes@gmail.com>
2026-03-18 20:36:34 -0300

    Teste de erro

--- calculadora.teste.js
+++ calculadora.teste.js
@@ -1,7 +1,7 @@
 const { soma, divide } = require("./calculadora");
 
 test("soma 2 + 3 = 5", () => {
-  expect(soma(2, 3)).toBe(5);
+  expect(soma(2, 3)).toBe(99); // errado
 });
 
 test("soma com negativo", () => {

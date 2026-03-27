44ac6ad33eb49d86322a569bbc0eeb96e0b41dc2
Guilherme Nunes <guilherme.ibnunes@gmail.com>
2026-03-27 13:02:41 -0300

    Adicionar imagem

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,7 +1,7 @@
 const { soma, divide } = require("./calculadora");
 
 test("soma 2 + 3 = 5", () => {
-  expect(soma(2, 3)).toBe(99); // certo
+  expect(soma(2, 3)).toBe(5); 
 });
 
 test("soma com negativo", () => {
